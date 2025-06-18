4fe09cfcfd74c08cba3537e8f8162d86f54aa111
Buddhika Madusanka <buddhikadevelopmetn@gmail.com>
2025-06-18 13:45:29 +0530

    Complete hero section with responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import HeroSection from "./components/layouts/HeroSection";
 import SecondUISection from "./components/layouts/SecondUISection";
+import SelfIntroductionSection from "./components/layouts/SelfIntroductionSection";
 
 export default function Home() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full overflow-y-scroll">
 
       {/* hero section */}
       <div className="lg:p-10 w-full h-full">
@@ -15,7 +16,12 @@ export default function Home() {
       <div className="px-10">
         <SecondUISection />
       </div>
-      
+
+
+      {/* self introduction section */}
+      <div className="px-10">
+        <SelfIntroductionSection />
+      </div>      
       
     </div>
   );
